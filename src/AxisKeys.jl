3fd925f826aecdd744cd0ba0c5b733f63281a867
brendanjohnharris <brendanjohnharris@gmail.com>
2022-06-04 19:42:47 +1000

    Make generalized_phase respect axis keys properly

--- src/AxisKeys.jl
+++ src/AxisKeys.jl
@@ -2,11 +2,15 @@ using AxisKeys
 
 function _generalized_phase(X::KeyedArray, dim=ndims(X), lp=0.0)
     # We assume the axis keys of the `dim` dimension are equally spaced (`Range`) time points.
+    @assert axiskeys(X, dim) isa AbstractRange
+    axs = axiskeys(X)
     fs = 1/step(axiskeys(X, dim))
-    mapslices(x->_generalized_phase(x, fs, lp), X; dims=dim)
+    KeyedArray(mapslices(x->_generalized_phase(x, fs, lp), X; dims=dim), axs)
 end
 
 function generalized_phase(X::KeyedArray, dim=ndims(X), lp=0.0)
+    @assert axiskeys(X, dim) isa AbstractRange
+    axs = axiskeys(X)
     fs = 1/step(axiskeys(X, dim))
-    mapslices(x->generalized_phase(x, fs, lp), X; dims=dim)
+    KeyedArray(mapslices(x->generalized_phase(x, fs, lp), X; dims=dim), axs)
 end
